ff5d7f361dd42981efa28a0ebe7ac35b2dcd2f06
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-14 11:48:30 +0100

    Commit 1

--- part1.js
+++ part1.js
@@ -42,3 +42,36 @@ const graph = {
 
 breadhtFirstPrint(graph, 'a');
 console.log(hasPath(graph, 'a', 'e'));
+
+
+const edges = [
+  ['i','j'],
+    ['k','i'],
+    ['n','k'],
+    ['k','l'],
+    ['o','n'],
+];
+
+
+const buildGraph = (edges) => {
+  const graph = {};
+    for (let edge of edges) {
+        const [a,b] = edge;
+        if (!(a in graph)){
+            graph[a] = [];
+        }
+        if (!(b in graph)){
+            graph[b] = [];
+        }
+
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    return graph;
+}
+
+const unDirectPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges);
+  return hasPath(graph, nodeA, nodeB);
+}
+unDirectPath(edges,'j','m');
