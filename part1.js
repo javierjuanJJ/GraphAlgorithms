588e29a733b06e3ce52e99f8784c630680f203c6
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-14 11:27:11 +0100

    Commit 1

--- part1.js
+++ part1.js
@@ -17,13 +17,14 @@ const breadhtFirstPrint = (graph, source) => {
 }
 
 const hasPath = (graph, src, dst) => {
-  if (src === dst){
-      return true;
-  }
-
-    for (let neighbor of graph[src]) {
-        if (hasPath(graph, src, dst) === true){
+  const queue = [ src ];
+    while (queue.length > 0){
+        const current = queue.shift();
+        if (current === dst){
             return true;
+        }
+        for (let neighbor of graph[current]) {
+            queue.push(neighbor);
         }
     }
     return false;
