89774d694e466b02c60e4f5178fd9e9246497f13
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-16 11:19:42 +0100

    Commit 1

--- part1.js
+++ part1.js
@@ -1,36 +1,34 @@
-const minimumIsland = (grid) => {
+const islandCount = (grid) => {
     const visited = new Set();
 
-    let minSize = Infinity;
+    let count = 0;
     for (let r = 0; r < grid.length; r += 1) {
         for (let c = 0; c < grid[0].length; c += 1) {
-            const size = exploreSize(grid, r, c, visited);
-            if (size > 0 && size < minSize) {
-                minSize = size;
+            if (explore(grid, r, c, visited) === true) {
+                count += 1;
             }
         }
     }
 
-    return minSize;
+    return count;
 };
 
-const exploreSize = (grid, r, c, visited) => {
+const explore = (grid, r, c, visited) => {
     const rowInbounds = 0 <= r && r < grid.length;
     const colInbounds = 0 <= c && c < grid[0].length;
-    if (!rowInbounds || !colInbounds) return 0;
+    if (!rowInbounds || !colInbounds) return false;
 
-    if (grid[r][c] === 'W') return 0;
+    if (grid[r][c] === 'W') return false;
 
     const pos = r + ',' + c;
-    if (visited.has(pos)) return 0;
+    if (visited.has(pos)) return false;
     visited.add(pos);
 
-    let size = 1;
-    size += exploreSize(grid, r - 1, c, visited);
-    size += exploreSize(grid, r + 1, c, visited);
-    size += exploreSize(grid, r, c - 1, visited);
-    size += exploreSize(grid, r, c + 1, visited);
-    return size;
+    explore(grid, r - 1, c, visited);
+    explore(grid, r + 1, c, visited);
+    explore(grid, r, c - 1, visited);
+    explore(grid, r, c + 1, visited);
+    return true;
 };
 
 const grid = [
@@ -41,4 +39,4 @@ const grid = [
     ['W', 'W', 'L', 'L', 'L'],
 ];
 
-console.log(minimumIsland(grid));
+console.log(islandCount(grid));
